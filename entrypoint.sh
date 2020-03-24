d5f9d537f5e3f3d87d3bc623de98543816e04996
Ville Korhonen <ville@xd.fi>
2020-03-24 19:04:59 +0200

    Add 'default' command for executing full build process

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,10 +42,27 @@ fi
 # FIXME: if SOURCE_DIR AND not /dev/null AND SOURCE_DIR/debian exists, build it
 # FIXME: if SOURCE_DIR not defined, read PACKAGE and apt-get source it
 
+gosrcdir() {
+    case "${BUILD_TYPE}"
+    in
+        package)
+	    cd /usr/src/"${PACKAGE}"-*
+	;;
+        source_dir)
+	    cd "${SOURCE_DIR}"
+	;;
+        *)
+	    echo "Invalid build type: '${BUILD_TYPE}'!"
+	    exit 1
+    esac
+}
+
 case "${COMMAND}"
 in
     collect-artifacts)
     	# FIXME: collect artifacts to /artifacts or something
+	gosrcdir
+	cd ..
 	if [ -e "/artifacts/.directory-not-mounted" ]
 	then
             echo "Found flag file, it seems that /artifacts isn't a volume. Skip copying artifacts."
@@ -55,6 +72,7 @@ in
     ;;
     install-deps)
         echo "Installing build dependencies..."
+	gosrcdir
 	/usr/lib/docker-helpers/apt-setup
         mk-build-deps --build-dep --install --remove --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
 	/usr/lib/docker-helpers/apt-cleanup
@@ -70,35 +88,37 @@ in
 	apt-get update
 	# FIXME: W: Download is performed unsandboxed as root as file 'nano_4.8-1.dsc' couldn't be accessed by user '_apt'. - pkgAcquire::Run (13: Permission denied)
 	apt-get source "${PACKAGE}"
-	# FIXME: how to deduce path from $PACKAGE ?
 	# or dgit or debcheckout?
+	# FIXME: how to deduce path from $PACKAGE ?
     ;;
     build)
     	# FIXME: unless using version forced by user, bump version
+	# FIXME: this should probably expect that src dir is already prepared, no need to get sources anymore...
+	gosrcdir
 	case "${BUILD_TYPE}"
 	in
 	    package)
 	        echo "Building from package sources '${PACKAGE}'..."
                 $0 get-source
-		cd $PACKAGE-*
        	    ;;
             source_dir)
                 echo "Building from source directory '${SOURCE_DIR}'..."
-		cd "${SOURCE_DIR}"
 	    ;;
             *)
 	    	echo "Invalid build type: '${BUILD_TYPE}'!"
 		exit 1
 	    ;;
 	esac
-	echo "Install build dependencies..."
-	$0 install-deps
 	chown user .. ../*.*
 	chown -R user .
 	ls -lha
 	ls -lha ..
     	gosu user dpkg-buildpackage -us -uc
 	find ../ -maxdepth 1 -type f -exec sha256sum "{}" \;
+    ;;
+    default)
+	$0 install-deps
+	$0 build
 	$0 collect-artifacts
     ;;
     *)
